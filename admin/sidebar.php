609d1cdc17e73bb609d85ba1d6e5bfd7c2055480
Chhayangba Lama <np03cs4a220016@heraldcollege.edu.np>
2024-05-15 14:22:53 +0545

    Updated sidebar.php

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -3,10 +3,17 @@
         <div class="menu-listing">
             <ul class="list">
                 <li><a href="<?php echo SITEURL; ?>admin" class="dashboard-icon"> Dashboard</a></li>
+                <li><a href="<?php echo SITEURL; ?>admin/invoice.php" class="dashboard-icon"> Invoice</a></li>
                 <li class="dropdown"><a href="#" class="dropdown-toggle employee-icon" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">Employee</a>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
                         <li class="dropdown-item"><a href="<?php echo SITEURL; ?>admin/add-admin.php">Add Employee</a></li>
                         <li class="dropdown-item"><a href="<?php echo SITEURL; ?>admin/manage-admin.php">Manage Employee</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown"><a href="#" class="dropdown-toggle employee-icon" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">Notice</a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <li class="dropdown-item"><a href="<?php echo SITEURL; ?>admin/add-notice.php">Add Notice</a></li>
+                        <li class="dropdown-item"><a href="<?php echo SITEURL; ?>admin/manage-notice.php">Manage Notice</a></li>
                     </ul>
                 </li>
             </ul>
